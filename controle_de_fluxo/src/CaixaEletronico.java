0760a9acc1ac7bb586392b1c578a4d71fd588ba7
maxcaetano <maxsuelcaetano@live.com>
2024-08-02 14:09:13 -0300

    Renomeação de variavel

--- controle_de_fluxo/src/CaixaEletronico.java
+++ controle_de_fluxo/src/CaixaEletronico.java
@@ -6,16 +6,16 @@ public class CaixaEletronico {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-        double valorParaSaque, saldo, novoSaldo;
+        double valorDeSaque, saldo, novoSaldo;
         String opcao = "S";
         saldo = 100.00;
 
         while (true) {
             if (saldo > 0) {
                 System.out.print("Digite o valor que deseja sacar: ");
-                valorParaSaque = scanner.nextDouble();
+                valorDeSaque = scanner.nextDouble();
 
-                novoSaldo = sacarValor(saldo, valorParaSaque);
+                novoSaldo = sacarValor(saldo, valorDeSaque);
                 saldo = novoSaldo;
 
                 System.out.println("Deseja continuar? [S/N]");
@@ -30,12 +30,12 @@ public class CaixaEletronico {
         }
     }
 
-    public static double sacarValor(double saldo, double valorParaSaque) {
+    public static double sacarValor(double saldo, double valorDeSaque) {
         double novoSaldo = 0;
 
-        if (saldo >= valorParaSaque) {
-            novoSaldo = saldo - valorParaSaque;
-            System.out.println("Valor " + valorParaSaque + " sacado com sucesso!");
+        if (saldo >= valorDeSaque) {
+            novoSaldo = saldo - valorDeSaque;
+            System.out.println("Valor " + valorDeSaque + " sacado com sucesso!");
             System.out.println("Saldo restante é de R$" + novoSaldo);
         } else {
             System.out.println("Saldo insuficiente, seu saldo atual é de R$" + saldo);
