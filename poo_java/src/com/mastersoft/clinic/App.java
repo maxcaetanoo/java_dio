43a84558717bceaf51742961707395d7cd38ef0e
maxcaetanoo <maxsuelcaetano@live.com>
2024-08-09 16:01:28 -0300

    Criação de Packages

---
+++ poo_java/src/com/mastersoft/clinic/App.java
@@ -0,0 +1,7 @@
+package com.mastersoft.clinic;
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Olá!");		
+	}
+}
---
+++ poo_java/src/com/mastersoft/clinic/service/ExameService.java
@@ -0,0 +1,9 @@
+package com.mastersoft.clinic.service;
+
+import com.mastersoft.clinic.model.Exame;
+
+public class ExameService {
+	void salvarExameService(Exame exame) {
+				
+	}
+}
